2b100bfa57b9ecc7948315a1389751f30feb19c8
Harikesh Patel <43793294+kumarharikesh@users.noreply.github.com>
2022-10-12 20:09:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,76 @@
-// Step 1 - selecting all the elements
-var initialPrice = document.querySelector("#initial-price");
-var stocksQuantity = document.querySelector("#stocks-quantity");
-var currentPrice = document.querySelector("#current-price");
-var submitBtn = document.querySelector("#submit-btn");
-var outputBox = document.querySelector("#output-box");
+const initialPrice = document.querySelector("#initial-price");
+const stocksQuantity = document.querySelector("#stocks-quantity");
+const currentPrice = document.querySelector("#current-price");
+const outputTag = document.querySelector(".output");
+const reset = document.querySelector("#clear");
 
-// Ex-06
-submitBtn.addEventListener("click", submitHandler);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+async function calculateProfitLoss(initial, quantity, current) {
+
+  outputTag.innerText ="";
+  outputTag.style.textAlign = "left";
+  outputTag.setAttribute("placeholder", "Loading");
+  await sleep(500);
+  outputTag.setAttribute("placeholder", "Loading.");
+  await sleep(500);
+  outputTag.setAttribute("placeholder", "Loading..");
+  await sleep(500);
+  outputTag.setAttribute("placeholder", "Loading...");
+  await sleep(500);
+  outputTag.setAttribute("placeholder", "Loading..");
+  await sleep(500);
+  outputTag.setAttribute("placeholder", "Loading.");
+  await sleep(500);
+  outputTag.setAttribute("placeholder", "Loading");
+  await sleep(500);
+  outputTag.setAttribute("placeholder", "Loading.");
+  await sleep(500);
+  outputTag.setAttribute("placeholder", "Loading..");
+  await sleep(500);
+  outputTag.setAttribute("placeholder", "Loading...");
+  await sleep(500);
+
+  if (initial > current) {
+
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (1-((current * quantity) / (initial * quantity))) * 100;
+    lossPercentage = lossPercentage.toFixed(2);
+
+    outputTag.innerHTML = `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`;
+    outputTag.style.color = "red";
+
+  } else if (current > initial) {
+
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (((current * quantity) / (initial * quantity))-1) * 100;
+    profitPercentage = profitPercentage.toFixed(2);
+
+    outputTag.innerHTML = `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`;
+    outputTag.style.color = "green";
+    
+  } else {
+    outputTag.innerHTML = `No pain no gain and no gain no pain`;
+  }
+}
 
 function submitHandler() {
   var ip = Number(initialPrice.value);
   var qty = Number(stocksQuantity.value);
   var curr = Number(currentPrice.value);
 
-  calculateProfitAndLoss(ip, qty, curr);
+  calculateProfitLoss(ip, qty, curr);
 }
 
-// Ex-05
-function calculateProfitAndLoss(initial, quantity, current) {
-  if (initial > current) {
-    var loss = (initial - current) * quantity;
-    var lossPercentage = (loss / initial) * 100;
+reset.addEventListener("click", function(){
+  outputTag.innerText="";
+  outputTag.setAttribute("placeholder", "Output will be shown here...");
+  outputTag.style.textAlign = "left";
+});
 
-    showOutput(
-      `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
-    );
-  } else if (current > initial) {
-    var profit = (current - initial) * quantity;
-    var profitPercentage = (profit / initial) * 100;
-
-    showOutput(
-      `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
-    );
-  } else {
-    showOutput(`No pain no gain and no gain no pain`);
-  }
-}
-
-function showOutput(message) {
-  outputBox.innerHTML = message;
-}
+document.getElementById("calculate-profit-form").addEventListener("submit", function(event){
+  submitHandler();
+  event.preventDefault();
+});
